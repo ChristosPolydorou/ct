df2d37deec2989a22903c6bd481ab37ea6d384b9
mandarpawar51 <mandarpawar18@gmail.com>
2023-04-02 19:41:11 +0100

    UserManager

--- app/src/main/java/com/example/ct/UserManager.kt
+++ app/src/main/java/com/example/ct/UserManager.kt
@@ -2,4 +2,8 @@ package com.example.ct
 //TODO this class is responsible for checking if the user is signal, facilitator, or spark. we can do this (lazily) by asking them, or checking some context variables (e.g. walking speed). it then modifies the corresponding variables in the trigger manager. i would just start off with setting signal to true, the other two to false.
 class UserManager (user: User) {
     //TODO modify this class to check if what type of user the user is
+    // Initialize user types
+    var signal: Boolean = true
+    var facilitator: Boolean = false
+    var spark: Boolean = false
 }
