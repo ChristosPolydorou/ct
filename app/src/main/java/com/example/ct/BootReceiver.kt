98204ebf00d16b9076f17ad83e07e26664c73040
mandarpawar51 <mandarpawar18@gmail.com>
2023-04-02 19:49:36 +0100

    Bootreceiver

--- app/src/main/java/com/example/ct/BootReceiver.kt
+++ app/src/main/java/com/example/ct/BootReceiver.kt
@@ -3,13 +3,18 @@ package com.example.ct
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
+
         if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
+            Log.d("BootReceiver", "Received BOOT_COMPLETED broadcast.")
             val serviceIntent = Intent(context, WalkReminderService::class.java)
             context.startForegroundService(serviceIntent)
+            Log.d("BootReceiver", "Started WalkReminderService.")
+
         }
     }
 }
