00d95bfc4b01cf40851b6fa4623f72c385fffece
mandarpawar51 <mandarpawar18@gmail.com>
2023-04-12 23:12:03 +0100

    worked on GeolocationDataSource

--- app/src/main/java/com/example/ct/GeolocationDataSource.kt
+++ app/src/main/java/com/example/ct/GeolocationDataSource.kt
@@ -1,68 +1,92 @@
 package com.example.ct
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.LocationManager
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-//import com.google.android.libraries.places.api.Places
-//import com.mapbox.api.geocoding.v5.models.CarmenFeature
-//import com.mapbox.geojson.Point
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
-
-class GeolocationDataSource(private val cache: Cache, private val context: Context) :
+class GeolocationDataSource(private val cache: Cache, private val activity: Activity,
+                            private val context: Context) :
     DataSourceManager(cache, context) {
 
-    private var locationResult = mutableMapOf<String, Boolean>()
-    private val LOCATION_NEAR = "location_near"
+    private val LOCATION_HOME = "location_home"
+    private val LOCATION_NEAR_JOGGING_TRACK = "location_near_jogging_track"
+    private val LOCATION_GET_OFF_BUS = "location_get_off_bus"
+
+    private val fusedLocationClient: FusedLocationProviderClient =
+        LocationServices.getFusedLocationProviderClient(context)
 
     @SuppressLint("MissingPermission")
     override fun loadData() {
-        // TODO Get the user's current location
-        val locationProvider = LocationManager.NETWORK_PROVIDER
-        val locationManager = ContextCompat.getSystemService(context, LocationManager::class.java) as LocationManager
-        val location = locationManager.getLastKnownLocation(locationProvider)!!
-        locationManager.getLastKnownLocation(locationProvider)
+        // Check location permissions
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Request the permission
+            ActivityCompat.requestPermissions(
+                activity,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+            // Permission not granted, can't continue
+            return
+        }
 
-        // Set up the Mapbox Places client
-//        val placesClient = Places.createClient(context)
-
-        // Define the search parameters
-//        val searchOptions = CarmenChipOptions.builder()
-//            .addPlaceCategory(PlaceCategory.PARK)
-//            .proximity(Point.fromLngLat(location.longitude, location.latitude))
-//            .radius(10000) // Search radius in meters
-//            .build()
-//
-//        // Search for nearby points of interest
-//        placesClient.fetchPlace(searchOptions, object : CarmenCallback<expected<MutableList<CarmenFeature>, CarmenError> {
-//            override fun onError(error: CarmenError) {
-//                // Handle error
-//            }
-//
-//            override fun onResult(result: expected<MutableList<CarmenFeature>>) {
-//                if (result is expected.Success) {
-//                    val parkNearby = result.value.isNotEmpty()
-//                    locationResult[LOCATION_NEAR] = parkNearby
-//                } else {
-//                    locationResult[LOCATION_NEAR] = false
-//                }
-//
-//                // Call setCache to save the result
-//                setCache(locationResult[LOCATION_NEAR])
-//            }
-//        })
-
-        // TODO Call setCache to save the result (if the place is user's office, home or some park)
-
+        // Get the user's current location
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            if (location != null) {
+                // User's location retrieved, proceed to check location criteria
+                checkLocationCriteria(location)
+            } else {
+                // Unable to retrieve user's location, handle error
+            }
+        }
     }
 
+
+    private fun checkLocationCriteria(location: android.location.Location) {
+        val homeLocation = android.location.Location("").apply {
+            latitude =  37.7749/* add latitude of user's home location */
+                longitude = -122.4194/* add longitude of user's home location */
+        }
+        val joggingTrackLocation = android.location.Location("").apply {
+            latitude = 37.7694/* add latitude of nearest jogging track */
+                longitude =-122.4768/* add longitude of nearest jogging track */
+        }
+        val busStopLocation = android.location.Location("").apply {
+            latitude = 37.7833/* add latitude of user's bus stop */
+                longitude =-122.4214 /* add longitude of user's bus stop */
+        }
+
+        val isNearHome = location.distanceTo(homeLocation) < 500 // Distance in meters
+        val isNearJoggingTrack = location.distanceTo(joggingTrackLocation) < 1000 // Distance in meters
+        val isNearBusStop = location.distanceTo(busStopLocation) < 50 // Distance in meters
+
+        // Prompt user to take a walk based on location criteria
+        if (isNearHome || isNearJoggingTrack) {
+            // Suggest taking a walk
+            // ...
+        } else if (isNearBusStop) {
+            // Suggest getting off bus and walking to home
+            // ...
+        }
+
+        // Save results to cache
+        cache.set(LOCATION_HOME, isNearHome)
+        cache.set(LOCATION_NEAR_JOGGING_TRACK, isNearJoggingTrack)
+        cache.set(LOCATION_GET_OFF_BUS, isNearBusStop)
+    }
 
     override fun setCache(locationResult : Any) {
-        cache.set(LOCATION_NEAR, locationResult)
+        // Cache already set in checkLocationCriteria method
     }
-
     companion object {
-        private const val PLACE_AUTOCOMPLETE_REQUEST_CODE = 1
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1234
     }
 }
-
